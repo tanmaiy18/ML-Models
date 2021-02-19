bd543d303bc71052a8b3706e36a3c7c85d2bc343
tanmaiy_r <tanmaiy18@gmail.com>
2021-02-19 22:10:18 +0530

    Updated

--- IPL/IPL_Model.py
+++ IPL/IPL_Model.py
@@ -2,7 +2,7 @@
 '''
 * @FileName : IPL_Model.py
 * @Author : Tanmaiy Reddy Arikatla
-* @Brief : Given details of ipl teams and players to predict the total runs they scored .
+* @Brief : To predict the total runs scored by an IPL Team.
 * @Date : 17 FEB 2021
 *
 * Copyright (C) 2021
