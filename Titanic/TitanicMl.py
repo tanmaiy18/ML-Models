f3f8065f3555ed9ac2ef2967fe9375a741c784d3
tanmaiy_r <tanmaiy18@gmail.com>
2021-02-19 22:07:44 +0530

    Updated

--- Titanic/TitanicMl.py
+++ Titanic/TitanicMl.py
@@ -2,7 +2,7 @@
 '''
 * @FileName : TitanicML.py
 * @Author : Tanmaiy Reddy Arikatla
-* @Brief : To predict if a person will survive if he travles on TITANIC II and it sinks.
+* @Brief : To predict if a person will survive if he travles on TITANIC II after it sinks.
 * @Date : 18 DEC 2020
 *
 * Copyright (C) 2020
